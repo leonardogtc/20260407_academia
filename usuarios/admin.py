9701b1ddb7dbcb4961c10637d7f32ead868f814f
Leonardo G.T. da Conceicao <leonardogtc@gmail.com>
2026-04-18 14:27:30 -0300

    Configurando o Admin de Usuários (Integrantes)

--- usuarios/admin.py
+++ usuarios/admin.py
@@ -1,3 +1,27 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import Integrantes
+
+
+@admin.register(Integrantes)
+class IntegrantesAdmin(UserAdmin):
+    # Adicionar novos campos personalizados nsa telas de edição do admin
+    fieldsets = UserAdmin.fieldsets + (
+        ('Informações do Grupo', {
+            'fields': (
+                'graduacao', 'instrutor', 'status'
+            )
+        }),
+        ('Contato Pessoal', {
+            'fields': (
+                'data_nascimento', 'cpf', 'telefone'
+            )
+        })
+    )
+
+    list_display = ('username', 'get_full_name',
+                    'graduacao', 'instrutor', 'status')
+    list_filter = ('graduacao', 'status')
+
 
 # Register your models here.
