f0b574f4690e7e267258e65f7d817f663c4b8b73
Leonardo G.T. da Conceicao <leonardogtc@gmail.com>
2026-04-18 14:21:20 -0300

    Configurando o Admin de Hierarquia

--- hierarquia/admin.py
+++ hierarquia/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import Modalidade, Graduacao
 
-# Register your models here.
+
+@admin.register(Modalidade)
+class ModalidadeAdmin(admin.ModelAdmin):
+    list_display = ('nome',)
+
+
+@admin.register(Graduacao)
+class GraduacaoAdmin(admin.ModelAdmin):
+    list_display = ('nome', 'modalidade', 'ordem',
+                    'pode_ter_alunos', 'valor_graduacao')
+    list_filter = ('modalidade', 'pode_ter_alunos')
+    search_fields = ('nome',)
