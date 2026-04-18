0fea5ae9bc5c6d1d4507cbcc34692154b8f5c20c
Leonardo G.T. da Conceicao <leonardogtc@gmail.com>
2026-04-18 18:24:57 -0300

    Configurar o Admin de Certificados

--- certificados/admin.py
+++ certificados/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import Certificado
 
-# Register your models here.
+
+@admin.register(Certificado)
+class CertificadoAdmin(admin.ModelAdmin):
+    # Exibimos a chave de autenticação na lista para fácil consulta
+    list_display = ('chave_autenticacao', 'integrante',
+                    'graduacao_alcancada', 'data_emissao',
+                    'foi_impresso_fisicamente')
+    list_filter = ('graduacao_alcancada', 'foi_impresso_fisicamente')
+    search_fields = ('integrante__first_name', 'chave_autenticacao')
+
+    # A chave é gerada automaticamente, então não a exibimos no formulário
+    # de edição
+    readonly_fields = ('chave_autenticacao', 'data_emissao')
