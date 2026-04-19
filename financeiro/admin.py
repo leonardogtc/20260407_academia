5dad6c8521fa24af90b74502f5ed6b9bc838fab0
Leonardo G.T. da Conceicao <leonardogtc@gmail.com>
2026-04-19 13:05:32 -0300

    Integrando tudo no Admin

--- financeiro/admin.py
+++ financeiro/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
+from .models import CategoriaDespesa, Despesa, PagamentoGraduacao
 
-# Register your models here.
+
+@admin.register(Despesa)
+class DespesaAdmin(admin.ModelAdmin):
+    list_display = ('categoria', 'descricao', 'valor_total',
+                    'data_vencimento', 'paga')
+    list_filter = ('paga', 'data_vencimento', 'categoria')
+    list_editable = ('paga',)
+    search_fields = ('descricao',)
+
+
+@admin.register(PagamentoGraduacao)
+class PagamentoGraduacaoAdmin(admin.ModelAdmin):
+    list_display = ('aluno', 'valor', 'data_pagamento', 'confirmado')
+    list_filter = ('confirmado',)
+    search_fields = ('aluno__first_name', 'aluno__username')
+
+
+admin.site.register(CategoriaDespesa)
