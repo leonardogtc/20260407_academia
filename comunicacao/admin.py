08f1cfe07933d4e168ecaa95a58eda9d8ebd4d29
Leonardo G.T. da Conceicao <leonardogtc@gmail.com>
2026-04-19 10:57:39 -0300

    Admin com "Inline" (Momento Tutoria)

--- comunicacao/admin.py
+++ comunicacao/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
+from .models import Evento, FotoEvento, Aviso
 
-# Register your models here.
+
+class FotoEventoInline(admin.TabularInline):
+    model = FotoEvento
+    extra = 1
+
+
+@admin.register(Evento)
+class EventoAdmin(admin.ModelAdmin):
+    list_display = ('titulo', 'data_evento', 'local', 'publicado', 'fechado')
+    list_filter = ('publicado', 'fechado', 'data_evento')
+    inlines = [FotoEventoInline]
+
+
+@admin.register(Aviso)
+class AvisoAdmin(admin.ModelAdmin):
+    list_display = ('titulo', 'autor', 'publicado', 'data_postagem')
+    list_filter = ('publicado', 'data_postagem', 'autor')
+    search_fields = ('titulo', 'texto')
